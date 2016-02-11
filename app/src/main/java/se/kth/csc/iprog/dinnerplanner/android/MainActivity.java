9891ddf46b90aab46196ae539739cb89b120b52a
martin <martin.funkquist@gmail.com>
2016-02-11 17:18:59 +0100

    Fixed nothing

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -6,21 +6,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
+import se.kth.csc.iprog.dinnerplanner.android.view.WelcomeScreen;
 
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Default call to load previous state
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Default call to load previous state
+    super.onCreate(savedInstanceState);
 
-        // Set the view for the main activity screen
-        // it must come before any call to findViewById method
-        setContentView(R.layout.activity_main);
+    // Set the view for the main activity screen
+    // it must come before any call to findViewById method
+    setContentView(R.layout.activity_welcome_screen);
 
-        // Creating the view class instance
-        ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
-    }
+    // Creating the view class instance
+ //   ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
+    WelcomeScreen mainView = new WelcomeScreen(findViewById(R.id.welcome_screen));
+  }
 
 }
---
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/WelcomeScreen.java
@@ -0,0 +1,18 @@
+package se.kth.csc.iprog.dinnerplanner.android.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import se.kth.csc.iprog.dinnerplanner.android.R;
+
+
+public class WelcomeScreen{
+
+  View view;
+
+  public WelcomeScreen(View view) {
+    this.view = view;
+
+  }
+}
