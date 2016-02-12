a5b725e31d86564b3841a552c0f188634db1320c
Martin <martin.funkquist@gmail.com>
2016-02-12 21:02:58 +0100

    Fixed welcome screen

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
 import se.kth.csc.iprog.dinnerplanner.android.view.WelcomeScreen;
@@ -15,14 +16,14 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     // Default call to load previous state
     super.onCreate(savedInstanceState);
-
+    this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
     // Set the view for the main activity screen
     // it must come before any call to findViewById method
     setContentView(R.layout.activity_welcome_screen);
-
+    this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
     // Creating the view class instance
  //   ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
-    WelcomeScreen mainView = new WelcomeScreen(findViewById(R.id.welcome_screen));
+ //   WelcomeScreen mainView = new WelcomeScreen(findViewById(R.id.welcome_screen));
   }
 
 }
